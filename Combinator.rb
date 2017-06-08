7410df091e3484edf339ae223dc1fba1a2afe191
Florian Meißner <developer@mystler.eu>
2017-06-08 13:27:19 +0200

    Make Combinator script interactive

--- Combinator.rb
+++ Combinator.rb
@@ -1,5 +1,5 @@
 def SimcLogToCSV(infile, outfile)
-  puts "Converting #{infile} to CSV..."
+  puts "Adding data from #{infile} to #{outfile}..."
   sims = { }
 
   # Read results
@@ -22,18 +22,73 @@ def SimcLogToCSV(infile, outfile)
   end
 end
 
-File.delete('Combinator.csv') if File.exist?('Combinator.csv')
-(1..3).each do |t1|
-  (1..3).each do |t2|
-    (1..3).each do |t3|
-      (1..3).each do |t6|
-        (1..3).each do |t7|
-          system "cd .. && simc $(tbuild)=#{t1}#{t2}#{t3}00#{t6}#{t7} scripts/Combinator.simc"
-          SimcLogToCSV('Combinator.log', 'Combinator.csv')
+# Select Combinator template profile
+puts 'Please choose the Combinator profile you want to simulate:'
+profiles = {}
+index = 1
+Dir.glob('Combinator_[a-zA-Z]*\.simc').each do |combfile|
+  if profile = combfile.match(/Combinator_([a-zA-Z]*)\.simc/)
+    profiles[index] = profile[1]
+    puts "#{index}: #{profile[1]}"
+    index += 1
+  end
+end
+print 'Profile: '
+index = gets.to_i
+unless profiles.has_key?(index)
+  raise 'Invalid profile index entered!'
+end
+puts
+
+# Select talents for permutation
+puts 'Please select the talents for permutation:'
+puts 'Options: 0-off, 1-left, 2-middle, 3-right, x-Permutation'
+puts 'Example: xxx00xx'
+print 'Talents: '
+talentstring = gets
+unless talentstring.match(/\A[0-3xX]{7}\Z/)
+  raise 'Invalid talent string!'
+end
+talentdata = []
+talentstring.chomp.each_char do |tier|
+  if tier.downcase.eql? 'x'
+    talentdata.push((1..3).to_a)
+  else
+    talentdata.push([tier.to_i])
+  end
+end
+puts
+
+# Recreate or append to csv?
+if File.exist?('Combinator.csv')
+  puts 'Do you want to delete the existing Combinator.csv file? If you type "n", results will be appended.'
+  print 'Y/n: '
+  deletefile = gets
+  unless deletefile.chomp.downcase.eql? 'n'
+    File.delete('Combinator.csv')
+    puts 'Old file deleted!'
+  end
+  puts
+end
+
+puts 'Starting simulations, this may take a while!'
+talentdata[0].each do |t1|
+  talentdata[1].each do |t2|
+    talentdata[2].each do |t3|
+      talentdata[3].each do |t4|
+        talentdata[4].each do |t5|
+          talentdata[5].each do |t6|
+            talentdata[6].each do |t7|
+              puts "Simulating talent string #{t1}#{t2}#{t3}#{t4}#{t5}#{t6}#{t7}..."
+              system "cd .. && simc $(tbuild)=#{t1}#{t2}#{t3}#{t4}#{t5}#{t6}#{t7} scripts/Combinator_#{profiles[index]}.simc"
+              SimcLogToCSV('Combinator.log', 'Combinator.csv')
+            end
+          end
         end
       end
     end
   end
 end
+puts
 puts 'Done! Press enter to quit...'
 gets
