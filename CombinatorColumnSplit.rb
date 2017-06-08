08bf99d239dfbaa2e51836f267f01d386e21ab7c
Florian Meißner <developer@mystler.eu>
2017-06-08 16:23:53 +0200

    Add script for chart column splitting

---
+++ CombinatorColumnSplit.rb
@@ -0,0 +1,18 @@
+lines = []
+filebase = File.basename(ARGV[0], ".csv")
+File.open(ARGV[0], 'r') do |file|
+  while line = file.gets
+    if data = line.match(/\A(\d+)_([^_]+)_?([^,]*),(.*)\Z/)
+      if not data[3].empty?
+        legos = data[3].gsub(/_/, ', ')
+      else
+        legos = 'None'
+      end
+      fullname = data[0].split(',')[0]
+      lines.push("#{fullname},#{data[1]},#{data[2]},\"#{legos}\",#{data[4]}")
+    end
+  end
+end
+File.open("#{filebase}_Converted.csv", "w") do |file|
+  lines.each { |line| file.puts line }
+end
