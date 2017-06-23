d7560146b61c40a27f5a973bbea860c88cad5574
Aethys256 <aethys256@users.noreply.github.com>
2017-06-23 17:03:58 +0200

    Add scripts to update all sims at once

---
+++ TCHubRelicSimulation.bat
@@ -0,0 +1,22 @@
+REM T19M
+echo 1 > relicSimulationInput.txt
+RelicSimulation.rb < relicSimulationInput.txt
+
+echo 3 > relicSimulationInput.txt
+RelicSimulation.rb < relicSimulationInput.txt
+
+echo 5 > relicSimulationInput.txt
+RelicSimulation.rb < relicSimulationInput.txt
+
+REM T20H
+echo 2 > relicSimulationInput.txt
+RelicSimulation.rb < relicSimulationInput.txt
+
+echo 4 > relicSimulationInput.txt
+RelicSimulation.rb < relicSimulationInput.txt
+
+echo 6 > relicSimulationInput.txt
+RelicSimulation.rb < relicSimulationInput.txt
+
+del relicSimulationInput.txt
+@pause
---
+++ TCHubTrinketSimulation.bat
@@ -0,0 +1,22 @@
+REM T19M
+echo 1 > trinketSimulationInput.txt
+RogueTrinkets.rb < trinketSimulationInput.txt
+
+echo 3 > trinketSimulationInput.txt
+RogueTrinkets.rb < trinketSimulationInput.txt
+
+echo 5 > trinketSimulationInput.txt
+RogueTrinkets.rb < trinketSimulationInput.txt
+
+REM T20H
+echo 2 > trinketSimulationInput.txt
+RogueTrinkets.rb < trinketSimulationInput.txt
+
+echo 4 > trinketSimulationInput.txt
+RogueTrinkets.rb < trinketSimulationInput.txt
+
+echo 6 > trinketSimulationInput.txt
+RogueTrinkets.rb < trinketSimulationInput.txt
+
+del trinketSimulationInput.txt
+@pause
