2ec073c517b7e23305a03ecf47e8314a0991e4b3
Florian Meißner <developer@mystler.eu>
2017-08-03 16:08:45 +0200

    Add fightstyles to Combinator website simulation batch After running this, assuming your PC didn't explode, it might at least ask for a vacation.

--- TCHubCombinatorUpdate.bat
+++ TCHubCombinatorUpdate.bat
@@ -3,9 +3,41 @@ Combinator.rb Assassination_T19M 1T xxx00xx y q
 Combinator.rb Outlaw_T19M 1T x3x00xx y q
 Combinator.rb Subtlety_T19M 1T xxx00xx y q
 
+Combinator.rb Assassination_T19M 2T xxx00xx y q
+Combinator.rb Outlaw_T19M 2T x3x00xx y q
+Combinator.rb Subtlety_T19M 2T xxx00xx y q
+
+Combinator.rb Assassination_T19M 3T xxx00xx y q
+Combinator.rb Outlaw_T19M 3T x3x00xx y q
+Combinator.rb Subtlety_T19M 3T xxx00xx y q
+
+Combinator.rb Assassination_T19M 1T_Adds xxx00xx y q
+Combinator.rb Outlaw_T19M 1T_Adds x3x00xx y q
+Combinator.rb Subtlety_T19M 1T_Adds xxx00xx y q
+
+Combinator.rb Assassination_T19M Mistress xxx00xx y q
+Combinator.rb Outlaw_T19M Mistress x3x00xx y q
+Combinator.rb Subtlety_T19M Mistress xxx00xx y q
+
 REM T20M
 Combinator.rb Assassination_T20M 1T xxx00xx y q
 Combinator.rb Outlaw_T20M 1T x3x00xx y q
 Combinator.rb Subtlety_T20M 1T xxx00xx y q
+
+Combinator.rb Assassination_T20M 2T xxx00xx y q
+Combinator.rb Outlaw_T20M 2T x3x00xx y q
+Combinator.rb Subtlety_T20M 2T xxx00xx y q
+
+Combinator.rb Assassination_T20M 3T xxx00xx y q
+Combinator.rb Outlaw_T20M 3T x3x00xx y q
+Combinator.rb Subtlety_T20M 3T xxx00xx y q
+
+Combinator.rb Assassination_T20M 1T_Adds xxx00xx y q
+Combinator.rb Outlaw_T20M 1T_Adds x3x00xx y q
+Combinator.rb Subtlety_T20M 1T_Adds xxx00xx y q
+
+Combinator.rb Assassination_T20M Mistress xxx00xx y q
+Combinator.rb Outlaw_T20M Mistress x3x00xx y q
+Combinator.rb Subtlety_T20M Mistress xxx00xx y q
 
 @pause
