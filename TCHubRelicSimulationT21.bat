2d586ab1532a06e6040ba4e89dc577d1b04920f5
Aethys256 <aethys256@users.noreply.github.com>
2017-11-03 13:04:18 +0100

    Revert "[Profiles] Adapt to recent simc changes"
    This reverts commit 28e74f94f74253399102b5ed6774367264d7ebc7.

--- TCHubRelicSimulationT21.bat
+++ TCHubRelicSimulationT21.bat
@@ -1,9 +1,9 @@
 REM T21M
 RelicSimulation.rb Assassination_T21M 1T q
-RelicSimulation.rb Assassination-Exsg_T21M 1T q
+RelicSimulation.rb Assassination_Exsg_T21M 1T q
 RelicSimulation.rb Outlaw_T21M 1T q
-RelicSimulation.rb Outlaw-SnD_T21M 1T q
+RelicSimulation.rb Outlaw_SnD_T21M 1T q
 RelicSimulation.rb Subtlety_T21M 1T q
-RelicSimulation.rb Subtlety-DfA_T21M 1T q
+RelicSimulation.rb Subtlety_DfA_T21M 1T q
 
 @pause
