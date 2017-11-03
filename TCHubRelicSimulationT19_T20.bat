28e74f94f74253399102b5ed6774367264d7ebc7
Aethys256 <aethys256@users.noreply.github.com>
2017-11-03 12:31:00 +0100

    [Profiles] Adapt to recent simc changes

--- TCHubRelicSimulationT19_T20.bat
+++ TCHubRelicSimulationT19_T20.bat
@@ -1,10 +1,10 @@
 REM T20M
 RelicSimulation.rb Assassination_T20M 1T q
-RelicSimulation.rb Assassination_Exsg_T20M 1T q
+RelicSimulation.rb Assassination-Exsg_T20M 1T q
 RelicSimulation.rb Outlaw_T20M 1T q
-RelicSimulation.rb Outlaw_SnD_T20M 1T q
+RelicSimulation.rb Outlaw-SnD_T20M 1T q
 RelicSimulation.rb Subtlety_T20M 1T q
-RelicSimulation.rb Subtlety_DfA_T20M 1T q
+RelicSimulation.rb Subtlety-DfA_T20M 1T q
 
 REM T19M_NH
 RelicSimulation.rb Assassination_T19M_NH 1T q
