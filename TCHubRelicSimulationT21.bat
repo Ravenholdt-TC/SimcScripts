a7456ddfde1aaba67d7e92e94f7a2a28f848a425
Aethys256 <aethys256@users.noreply.github.com>
2017-11-03 01:20:31 +0100

    [Scripts] Refactor .bat starters

---
+++ TCHubRelicSimulationT21.bat
@@ -0,0 +1,9 @@
+REM T21M
+RelicSimulation.rb Assassination_T21M 1T q
+RelicSimulation.rb Assassination_Exsg_T21M 1T q
+RelicSimulation.rb Outlaw_T21M 1T q
+RelicSimulation.rb Outlaw_SnD_T21M 1T q
+RelicSimulation.rb Subtlety_T21M 1T q
+RelicSimulation.rb Subtlety_DfA_T21M 1T q
+
+@pause
