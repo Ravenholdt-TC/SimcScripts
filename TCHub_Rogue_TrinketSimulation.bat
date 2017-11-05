2a4c7684bc457c682ab268f444d30c0c241ecb7c
Aethys256 <aethys256@users.noreply.github.com>
2017-11-05 21:16:34 +0100

    Name consistency update

--- TCHub_Rogue_TrinketSimulation.bat
+++ TCHub_Rogue_TrinketSimulation.bat
@@ -1,22 +1,22 @@
 REM T21
-TrinketSimulation.rb T21_Rogue_Assassination Rogue 1T q
-TrinketSimulation.rb T21_Rogue_Assassination_Exsg Rogue 1T q
-TrinketSimulation.rb T21_Rogue_Outlaw Rogue 1T q
-TrinketSimulation.rb T21_Rogue_Outlaw_SnD Rogue 1T q
-TrinketSimulation.rb T21_Rogue_Subtlety Rogue 1T q
-TrinketSimulation.rb T21_Rogue_Subtlety_DfA Rogue 1T q
+TrinketSimulation.rb T21_Rogue_Assassination Melee_Agillity 1T q
+TrinketSimulation.rb T21_Rogue_Assassination_Exsg Melee_Agillity 1T q
+TrinketSimulation.rb T21_Rogue_Outlaw Melee_Agillity 1T q
+TrinketSimulation.rb T21_Rogue_Outlaw_SnD Melee_Agillity 1T q
+TrinketSimulation.rb T21_Rogue_Subtlety Melee_Agillity 1T q
+TrinketSimulation.rb T21_Rogue_Subtlety_DfA Melee_Agillity 1T q
 
 REM T20
-TrinketSimulation.rb T20_Rogue_Assassination Rogue 1T q
-TrinketSimulation.rb T20_Rogue_Assassination_Exsg Rogue 1T q
-TrinketSimulation.rb T20_Rogue_Outlaw Rogue 1T q
-TrinketSimulation.rb T20_Rogue_Outlaw_SnD Rogue 1T q
-TrinketSimulation.rb T20_Rogue_Subtlety Rogue 1T q
-TrinketSimulation.rb T20_Rogue_Subtlety_DfA Rogue 1T q
+TrinketSimulation.rb T20_Rogue_Assassination Melee_Agillity 1T q
+TrinketSimulation.rb T20_Rogue_Assassination_Exsg Melee_Agillity 1T q
+TrinketSimulation.rb T20_Rogue_Outlaw Melee_Agillity 1T q
+TrinketSimulation.rb T20_Rogue_Outlaw_SnD Melee_Agillity 1T q
+TrinketSimulation.rb T20_Rogue_Subtlety Melee_Agillity 1T q
+TrinketSimulation.rb T20_Rogue_Subtlety_DfA Melee_Agillity 1T q
 
 REM T19
-TrinketSimulation.rb T19_Rogue_Assassination Rogue 1T q
-TrinketSimulation.rb T19_Rogue_Outlaw Rogue 1T q
-TrinketSimulation.rb T19_Rogue_Subtlety Rogue 1T q
+TrinketSimulation.rb T19_Rogue_Assassination Melee_Agillity 1T q
+TrinketSimulation.rb T19_Rogue_Outlaw Melee_Agillity 1T q
+TrinketSimulation.rb T19_Rogue_Subtlety Melee_Agillity 1T q
 
 @pause
