74c108ba99555965516f104258178808ff530087
Aethys256 <aethys256@users.noreply.github.com>
2017-11-12 14:54:43 +0100

    [Batch] Restore the destroyed combinator!

--- batch/TCHub_Rogue_Combinator.bat
+++ batch/TCHub_Rogue_Combinator.bat
@@ -2,14 +2,31 @@ cd ..
 
 REM T21
 Combinator.rb Rogue T21_Rogue_Assassination Rogue T21 1T xxx001x y q
-RelicSimulation.rb Warlock T21_Warlock_Demonology 1T q
-RelicSimulation.rb Warlock T20_Warlock_Demonology 1T q
-TrinketSimulation.rb Hunter T20_Hunter_Marksmanship Ranged_Agility 1T q
-TrinketSimulation.rb Hunter T21_Hunter_Marksmanship Ranged_Agility 1T q
-TrinketSimulation.rb Hunter T20_Hunter_Beast_Mastery Ranged_Agility 1T q
-TrinketSimulation.rb Hunter T21_Hunter_Beast_Mastery Ranged_Agility 1T q
-TrinketSimulation.rb Hunter T20_Hunter_Survival Melee_Agility 1T q
-TrinketSimulation.rb Hunter T21_Hunter_Survival Melee_Agility 1T q
+Combinator.rb Rogue T21_Rogue_Assassination Rogue T21 1T xxx002x n q
+Combinator.rb Rogue T21_Rogue_Assassination Rogue_Assassination_Exsg T21 1T xxx003x n q
+Combinator.rb Rogue T21_Rogue_Outlaw Rogue T21 1T x3x00xx y q
+Combinator.rb Rogue T21_Rogue_Subtlety Rogue T21 1T xxx00xx y q
 
+REM T21H
+Combinator.rb Rogue T21H_Rogue_Assassination Rogue T21H 1T xxx001x y q
+Combinator.rb Rogue T21H_Rogue_Assassination Rogue T21H 1T xxx002x n q
+Combinator.rb Rogue T21H_Rogue_Assassination Rogue_Assassination_Exsg T21H 1T xxx003x n q
+Combinator.rb Rogue T21H_Rogue_Outlaw Rogue T21H 1T x3x00xx y q
+Combinator.rb Rogue T21H_Rogue_Subtlety Rogue T21H 1T xxx00xx y q
+
+REM T20
+Combinator.rb Rogue T20_Rogue_Assassination Rogue T20 1T xxx00xx y q
+Combinator.rb Rogue T20_Rogue_Outlaw Rogue T20 1T x3x00xx y q
+Combinator.rb Rogue T20_Rogue_Subtlety Rogue T20 1T xxx00xx y q
+
+REM T19
+Combinator.rb Rogue T19_Rogue_Assassination Rogue T19 1T xxx00xx y q
+Combinator.rb Rogue T19_Rogue_Outlaw Rogue T19 1T x3x00xx y q
+Combinator.rb Rogue T19_Rogue_Subtlety Rogue T19 1T xxx00xx y q
+
+REM PR
+Combinator.rb Rogue PR_Rogue_Assassination Rogue PR 1T xxx00xx y q
+Combinator.rb Rogue PR_Rogue_Outlaw Rogue PR 1T x3x00xx y q
+Combinator.rb Rogue PR_Rogue_Subtlety Rogue PR 1T xxx00xx y q
 
 @pause
