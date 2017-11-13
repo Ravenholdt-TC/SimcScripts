3092fd2f7446bdb1cfe0ac37e259880e052abd4a
Nia <Nianel@users.noreply.github.com>
2017-11-14 00:37:10 +0100

    Batch All

---
+++ batch/TCHub_All_Combinator.bat
@@ -0,0 +1,23 @@
+cd ..
+
+REM T21
+TrinketSimulation.rb Monk T21_Monk_Windwalker Melee_Agility 1T q
+TrinketSimulation.rb Monk T20_Monk_Windwalker Melee_Agility 1T q
+TrinketSimulation.rb DemonHunter T21_Demon_Hunter_Havoc Melee_Agility 1T q
+TrinketSimulation.rb DemonHunter T20_Demon_Hunter_Havoc Melee_Agility 1T q
+TrinketSimulation.rb Druid T20_Druid_Feral Melee_Agility 1T q
+TrinketSimulation.rb Druid T21_Druid_Feral Melee_Agility 1T q
+TrinketSimulation.rb Shaman T20_Shaman_Enhancement Melee_Agility 1T q
+TrinketSimulation.rb Shaman T21_Shaman_Enhancement Melee_Agility 1T q
+TrinketSimulation.rb Hunter T20_Hunter_Survival Melee_Agility 1T q
+TrinketSimulation.rb Hunter T21_Hunter_Survival Melee_Agility 1T q
+
+REM T21H
+
+
+
+REM T20
+
+
+
+@pause
