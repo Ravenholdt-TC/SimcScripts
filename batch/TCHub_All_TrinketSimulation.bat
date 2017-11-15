5bba8874cfed7ba450b8151627ec85ee9197fc69
Aethys256 <aethys256@users.noreply.github.com>
2017-11-15 06:39:58 +0100

    [Batch] All TrinketSimulation

---
+++ batch/TCHub_All_TrinketSimulation.bat
@@ -0,0 +1,49 @@
+cd ..
+
+REM T21
+TrinketSimulation.rb DeathKnight T21_Death_Knight_Frost Melee_Strength 1T q
+TrinketSimulation.rb DeathKnight T21_Death_Knight_Unholy Melee_Strength 1T q
+TrinketSimulation.rb DemonHunter T21_Demon_Hunter_Havoc Melee_Agility 1T q
+TrinketSimulation.rb Druid T21_Druid_Balance Ranged_Intelligence 1T q
+TrinketSimulation.rb Druid T21_Druid_Feral Melee_Agility 1T q
+TrinketSimulation.rb Hunter T21_Hunter_Beast_Mastery Ranged_Agility 1T q
+TrinketSimulation.rb Hunter T21_Hunter_Marksmanship Ranged_Agility 1T q
+TrinketSimulation.rb Hunter T21_Hunter_Survival Melee_Agility 1T q
+TrinketSimulation.rb Mage T21_Mage_Arcane Ranged_Intelligence 1T q
+TrinketSimulation.rb Mage T21_Mage_Fire Ranged_Intelligence 1T q
+TrinketSimulation.rb Mage T21_Mage_Frost Ranged_Intelligence 1T q
+TrinketSimulation.rb Monk T21_Monk_Windwalker Melee_Agility 1T q
+TrinketSimulation.rb Paladin T21_Paladin_Retribution Melee_Strength 1T q
+TrinketSimulation.rb Priest T21_Priest_Shadow Ranged_Intelligence 1T q
+TrinketSimulation.rb Shaman T21_Shaman_Elemental Ranged_Intelligence 1T q
+TrinketSimulation.rb Shaman T21_Shaman_Enhancement Melee_Agility 1T q
+TrinketSimulation.rb Warlock T21_Warlock_Affliction Ranged_Intelligence 1T q
+TrinketSimulation.rb Warlock T21_Warlock_Demonology Ranged_Intelligence 1T q
+TrinketSimulation.rb Warlock T21_Warlock_Destruction Ranged_Intelligence 1T q
+TrinketSimulation.rb Warrior T21_Warrior_Arms Melee_Strength 1T q
+TrinketSimulation.rb Warrior T21_Warrior_Fury Melee_Strength 1T q
+
+REM T20
+TrinketSimulation.rb DeathKnight T20_Death_Knight_Frost Melee_Strength 1T q
+TrinketSimulation.rb DeathKnight T20_Death_Knight_Unholy Melee_Strength 1T q
+TrinketSimulation.rb DemonHunter T20_Demon_Hunter_Havoc Melee_Agility 1T q
+TrinketSimulation.rb Druid T20_Druid_Balance Ranged_Intelligence 1T q
+TrinketSimulation.rb Druid T20_Druid_Feral Melee_Agility 1T q
+TrinketSimulation.rb Hunter T20_Hunter_Beast_Mastery Ranged_Agility 1T q
+TrinketSimulation.rb Hunter T20_Hunter_Marksmanship Ranged_Agility 1T q
+TrinketSimulation.rb Hunter T20_Hunter_Survival Melee_Agility 1T q
+TrinketSimulation.rb Mage T20_Mage_Arcane Ranged_Intelligence 1T q
+TrinketSimulation.rb Mage T20_Mage_Fire Ranged_Intelligence 1T q
+TrinketSimulation.rb Mage T20_Mage_Frost Ranged_Intelligence 1T q
+TrinketSimulation.rb Monk T20_Monk_Windwalker Melee_Agility 1T q
+TrinketSimulation.rb Paladin T20_Paladin_Retribution Melee_Strength 1T q
+TrinketSimulation.rb Priest T20_Priest_Shadow Ranged_Intelligence 1T q
+TrinketSimulation.rb Shaman T20_Shaman_Elemental Ranged_Intelligence 1T q
+TrinketSimulation.rb Shaman T20_Shaman_Enhancement Melee_Agility 1T q
+TrinketSimulation.rb Warlock T20_Warlock_Affliction Ranged_Intelligence 1T q
+TrinketSimulation.rb Warlock T20_Warlock_Demonology Ranged_Intelligence 1T q
+TrinketSimulation.rb Warlock T20_Warlock_Destruction Ranged_Intelligence 1T q
+TrinketSimulation.rb Warrior T20_Warrior_Arms Melee_Strength 1T q
+TrinketSimulation.rb Warrior T20_Warrior_Fury Melee_Strength 1T q
+
+@pause
