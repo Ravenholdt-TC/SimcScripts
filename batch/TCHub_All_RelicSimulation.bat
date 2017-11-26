b3ba3d8b26b61d86871dfe2a4b7338f15101219f
Nia <Nianel@users.noreply.github.com>
2017-11-27 00:13:58 +0100

    [Batch] All relic

---
+++ batch/TCHub_All_RelicSimulation.bat
@@ -0,0 +1,52 @@
+cd ..
+
+REM T21
+RelicSimulation.rb DeathKnight T21_Death_Knight_Frost 1T q
+RelicSimulation.rb DeathKnight T21_Death_Knight_Unholy 1T q
+RelicSimulation.rb DemonHunter T21_Demon_Hunter_Havoc 1T q
+RelicSimulation.rb Druid T21_Druid_Balance 1T q
+RelicSimulation.rb Druid T21_Druid_Feral 1T q
+RelicSimulation.rb Hunter T21_Hunter_Beast_Mastery 1T q
+RelicSimulation.rb Hunter T21_Hunter_Beast_Mastery_Stomp 1T q
+RelicSimulation.rb Hunter T21_Hunter_Marksmanship 1T q
+RelicSimulation.rb Hunter T21_Hunter_Survival 1T q
+RelicSimulation.rb Mage T21_Mage_Arcane 1T q
+RelicSimulation.rb Mage T21_Mage_Fire 1T q
+RelicSimulation.rb Mage T21_Mage_Frost 1T q
+RelicSimulation.rb Mage T21_Monk_Windwalker 1T q
+RelicSimulation.rb Mage T21_Paladin_Retribution 1T q
+RelicSimulation.rb Mage T21_Priest_Shadow 1T q
+RelicSimulation.rb Shaman T21_Shaman_Elemental 1T q
+RelicSimulation.rb Shaman T21_Shaman_Enhancement 1T q
+RelicSimulation.rb Shaman T21_Warlock_Affliction 1T q
+RelicSimulation.rb Shaman T21_Warlock_Demonology 1T q
+RelicSimulation.rb Shaman T21_Warlock_Destruction 1T q
+RelicSimulation.rb Warrior T21_Warrior_Arms 1T q
+RelicSimulation.rb Warrior T21_Warrior_Fury 1T q
+
+REM T20
+RelicSimulation.rb DeathKnight T20_Death_Knight_Frost 1T q
+RelicSimulation.rb DeathKnight T20_Death_Knight_Unholy 1T q
+RelicSimulation.rb DemonHunter T20_Demon_Hunter_Havoc 1T q
+RelicSimulation.rb Druid T20_Druid_Balance 1T q
+RelicSimulation.rb Druid T20_Druid_Feral 1T q
+RelicSimulation.rb Hunter T20_Hunter_Beast_Mastery 1T q
+RelicSimulation.rb Hunter T20_Hunter_Beast_Mastery_Stomp 1T q
+RelicSimulation.rb Hunter T20_Hunter_Marksmanship 1T q
+RelicSimulation.rb Hunter T20_Hunter_Survival 1T q
+RelicSimulation.rb Mage T20_Mage_Arcane 1T q
+RelicSimulation.rb Mage T20_Mage_Fire 1T q
+RelicSimulation.rb Mage T20_Mage_Frost 1T q
+RelicSimulation.rb Mage T20_Monk_Windwalker 1T q
+RelicSimulation.rb Mage T20_Paladin_Retribution 1T q
+RelicSimulation.rb Mage T20_Priest_Shadow 1T q
+RelicSimulation.rb Shaman T20_Shaman_Elemental 1T q
+RelicSimulation.rb Shaman T20_Shaman_Enhancement 1T q
+RelicSimulation.rb Shaman T20_Warlock_Affliction 1T q
+RelicSimulation.rb Shaman T20_Warlock_Demonology 1T q
+RelicSimulation.rb Shaman T20_Warlock_Destruction 1T q
+RelicSimulation.rb Warrior T20_Warrior_Arms 1T q
+RelicSimulation.rb Warrior T20_Warrior_Fury 1T q
+
+
+@pause
