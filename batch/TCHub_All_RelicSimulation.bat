93d09564e7305dac67bb22b884acba94d607b6d6
Aethys256 <aethys256@users.noreply.github.com>
2017-11-27 08:29:17 +0100

    [Batch] Fix AIO relics

--- batch/TCHub_All_RelicSimulation.bat
+++ batch/TCHub_All_RelicSimulation.bat
@@ -13,14 +13,14 @@ RelicSimulation.rb Hunter T21_Hunter_Survival 1T q
 RelicSimulation.rb Mage T21_Mage_Arcane 1T q
 RelicSimulation.rb Mage T21_Mage_Fire 1T q
 RelicSimulation.rb Mage T21_Mage_Frost 1T q
-RelicSimulation.rb Mage T21_Monk_Windwalker 1T q
-RelicSimulation.rb Mage T21_Paladin_Retribution 1T q
-RelicSimulation.rb Mage T21_Priest_Shadow 1T q
+RelicSimulation.rb Monk T21_Monk_Windwalker 1T q
+RelicSimulation.rb Paladin T21_Paladin_Retribution 1T q
+RelicSimulation.rb Priest T21_Priest_Shadow 1T q
 RelicSimulation.rb Shaman T21_Shaman_Elemental 1T q
 RelicSimulation.rb Shaman T21_Shaman_Enhancement 1T q
-RelicSimulation.rb Shaman T21_Warlock_Affliction 1T q
-RelicSimulation.rb Shaman T21_Warlock_Demonology 1T q
-RelicSimulation.rb Shaman T21_Warlock_Destruction 1T q
+RelicSimulation.rb Warlock T21_Warlock_Affliction 1T q
+RelicSimulation.rb Warlock T21_Warlock_Demonology 1T q
+RelicSimulation.rb Warlock T21_Warlock_Destruction 1T q
 RelicSimulation.rb Warrior T21_Warrior_Arms 1T q
 RelicSimulation.rb Warrior T21_Warrior_Fury 1T q
 
@@ -37,14 +37,14 @@ RelicSimulation.rb Hunter T20_Hunter_Survival 1T q
 RelicSimulation.rb Mage T20_Mage_Arcane 1T q
 RelicSimulation.rb Mage T20_Mage_Fire 1T q
 RelicSimulation.rb Mage T20_Mage_Frost 1T q
-RelicSimulation.rb Mage T20_Monk_Windwalker 1T q
-RelicSimulation.rb Mage T20_Paladin_Retribution 1T q
-RelicSimulation.rb Mage T20_Priest_Shadow 1T q
+RelicSimulation.rb Monk T20_Monk_Windwalker 1T q
+RelicSimulation.rb Paladin T20_Paladin_Retribution 1T q
+RelicSimulation.rb Priest T20_Priest_Shadow 1T q
 RelicSimulation.rb Shaman T20_Shaman_Elemental 1T q
 RelicSimulation.rb Shaman T20_Shaman_Enhancement 1T q
-RelicSimulation.rb Shaman T20_Warlock_Affliction 1T q
-RelicSimulation.rb Shaman T20_Warlock_Demonology 1T q
-RelicSimulation.rb Shaman T20_Warlock_Destruction 1T q
+RelicSimulation.rb Warlock T20_Warlock_Affliction 1T q
+RelicSimulation.rb Warlock T20_Warlock_Demonology 1T q
+RelicSimulation.rb Warlock T20_Warlock_Destruction 1T q
 RelicSimulation.rb Warrior T20_Warrior_Arms 1T q
 RelicSimulation.rb Warrior T20_Warrior_Fury 1T q
 
