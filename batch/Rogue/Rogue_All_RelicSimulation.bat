406275ecbdd8beae931dc921e8052a9dcc7ad251
Florian Meißner <developer@mystler.eu>
2017-12-19 11:45:57 +0100

    [Relics/Rogue] Add Assa FoK profiles

--- batch/Rogue/Rogue_All_RelicSimulation.bat
+++ batch/Rogue/Rogue_All_RelicSimulation.bat
@@ -2,7 +2,10 @@ setlocal
 cd %~dp0\..\..
 
 REM T21
-RelicSimulation.rb Rogue T21_Rogue_Assassination 1T q
+RelicSimulation.rb Rogue T21_Rogue_Assassination-Mantle+Bracers 1T q
+RelicSimulation.rb Rogue T21_Rogue_Assassination-Mantle+Bracers_FoK 1T q
+RelicSimulation.rb Rogue T21_Rogue_Assassination-T21_4+T20_2_Bracers+Boots 1T q
+RelicSimulation.rb Rogue T21_Rogue_Assassination-T21_4+T20_2_Bracers+Boots_FoK 1T q
 RelicSimulation.rb Rogue T21_Rogue_Assassination_Exsg 1T q
 RelicSimulation.rb Rogue T21_Rogue_Outlaw 1T q
 RelicSimulation.rb Rogue T21_Rogue_Outlaw_SnD 1T q
