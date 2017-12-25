0dbc0921ba2bdf6a573736c0adaa358d6932cf82
Aethys256 <aethys256@users.noreply.github.com>
2017-12-25 14:48:01 +0100

    [JSONParser] WrilteFilePretty

--- lib/JSONParser.rb
+++ lib/JSONParser.rb
@@ -15,6 +15,13 @@ module JSONParser
   # Write a hash into a JSON file
   def self.WriteFile(filename, hash)
     File.open(filename, 'w') do |file|
+      file.write JSON.generate(hash)
+    end
+  end
+
+  # Write a hash into a JSON file
+  def self.WriteFilePretty(filename, hash)
+    File.open(filename, 'w') do |file|
       file.write JSON.pretty_generate(hash)
     end
   end
