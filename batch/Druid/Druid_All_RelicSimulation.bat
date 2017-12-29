fe718ae08b5ec429030b99e6c9536149157c756d
Aethys256 <aethys256@users.noreply.github.com>
2017-12-29 12:58:51 +0100

    [Druid] T21 Feral Incarn & SOTF

--- batch/Druid/Druid_All_RelicSimulation.bat
+++ batch/Druid/Druid_All_RelicSimulation.bat
@@ -5,6 +5,8 @@ REM T21
 RelicSimulation.rb Druid T21_Druid_Balance Druid_Balance 1T q
 RelicSimulation.rb Druid T21_Druid_Balance-Stellar_Drift Druid_Balance 1T q
 RelicSimulation.rb Druid T21_Druid_Feral Druid_Feral 1T q
+RelicSimulation.rb Druid T21_Druid_Feral-Incarn Druid_Feral 1T q
+RelicSimulation.rb Druid T21_Druid_Feral-SotF Druid_Feral 1T q
 
 REM T20
 RelicSimulation.rb Druid T20_Druid_Balance Druid_Balance 1T q
