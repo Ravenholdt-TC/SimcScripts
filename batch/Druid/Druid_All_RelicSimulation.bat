3b13f4035b7f257b3f1bedf15a6905264c1e147e
Aethys256 <aethys256@users.noreply.github.com>
2017-12-29 15:28:21 +0100

    [Druid] Rename feral relics profiles

--- batch/Druid/Druid_All_RelicSimulation.bat
+++ batch/Druid/Druid_All_RelicSimulation.bat
@@ -4,9 +4,9 @@ cd %~dp0\..\..
 REM T21
 RelicSimulation.rb Druid T21_Druid_Balance Druid_Balance 1T q
 RelicSimulation.rb Druid T21_Druid_Balance-Stellar_Drift Druid_Balance 1T q
-RelicSimulation.rb Druid T21_Druid_Feral Druid_Feral 1T q
-RelicSimulation.rb Druid T21_Druid_Feral-Incarn Druid_Feral 1T q
-RelicSimulation.rb Druid T21_Druid_Feral-SotF Druid_Feral 1T q
+RelicSimulation.rb Druid T21_Druid_Feral-BS_+_BT Druid_Feral 1T q
+RelicSimulation.rb Druid T21_Druid_Feral-LI_+_Incarnation Druid_Feral 1T q
+RelicSimulation.rb Druid T21_Druid_Feral-LI_+_Soul_of_the_Forest Druid_Feral 1T q
 
 REM T20
 RelicSimulation.rb Druid T20_Druid_Balance Druid_Balance 1T q
