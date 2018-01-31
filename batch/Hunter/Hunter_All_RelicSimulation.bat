96222a98706d14b8ec314b28ed13e4c70cc812d8
Nia <Nianel@users.noreply.github.com>
2018-01-31 16:46:29 +0100

    [Batch] Add Marksman Sentinel profil

--- batch/Hunter/Hunter_All_RelicSimulation.bat
+++ batch/Hunter/Hunter_All_RelicSimulation.bat
@@ -7,6 +7,7 @@ RelicSimulation.rb Hunter T21_Hunter_Beast_Mastery_Stomp Hunter_Beast_Mastery 1T
 RelicSimulation.rb Hunter T21_Hunter_Beast_Mastery_Stomp-T20-4+T21-2_AotB Hunter_Beast_Mastery 1T q
 RelicSimulation.rb Hunter T21_Hunter_Beast_Mastery_Stomp-T20-4+T21-2_KC Hunter_Beast_Mastery 1T q
 RelicSimulation.rb Hunter T21_Hunter_Marksmanship Hunter_Marksmanship 1T q
+RelicSimulation.rb Hunter T21_Hunter_Marksmanship-Sentinel Hunter_Marksmanship1T q
 RelicSimulation.rb Hunter T21_Hunter_Survival Hunter_Survival 1T q
 
 REM T20
