4a14f11f3b8efecf2b5e3773cc79ee25c48d021a
Nia <Nianel@users.noreply.github.com>
2018-02-17 00:06:06 +0100

    [Combinator] Add all legendaries Tank

--- batch/Paladin/Paladin_All_Combinator.bat
+++ batch/Paladin/Paladin_All_Combinator.bat
@@ -2,10 +2,10 @@ setlocal
 cd %~dp0\..\..
 
 REM T21
-Combinator.rb Paladin T21_Paladin_Retribution Paladin_Retribution T21 1T xx0x00x q
+Combinator.rb Paladin T21_Paladin_Retribution Paladin T21 1T xx0x00x q
 
 REM T21H
-Combinator.rb Paladin T21H_Paladin_Retribution Paladin_Retribution T21H 1T xx0x00x q
+Combinator.rb Paladin T21H_Paladin_Retribution Paladin T21H 1T xx0x00x q
 
 @if "%1"=="nopause" goto finish
 @pause
