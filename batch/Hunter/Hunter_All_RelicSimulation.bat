e45621d43bd5941516d12f1fbcb8dffbaf8093fd
aethys256 <aethys256@users.noreply.github.com>
2018-04-01 16:30:27 +0200

    [Batch] Fix Hunter MM Sentinel fightstyle

--- batch/Hunter/Hunter_All_RelicSimulation.bat
+++ batch/Hunter/Hunter_All_RelicSimulation.bat
@@ -10,7 +10,7 @@ RelicSimulation.rb Hunter T21_Hunter_Beast_Mastery_Stomp Hunter_Beast_Mastery %f
 RelicSimulation.rb Hunter T21_Hunter_Beast_Mastery_Stomp-T20-4+T21-2_AotB Hunter_Beast_Mastery %fightstyle% q
 RelicSimulation.rb Hunter T21_Hunter_Beast_Mastery_Stomp-T20-4+T21-2_KC Hunter_Beast_Mastery %fightstyle% q
 RelicSimulation.rb Hunter T21_Hunter_Marksmanship Hunter_Marksmanship %fightstyle% q
-RelicSimulation.rb Hunter T21_Hunter_Marksmanship-Sentinel Hunter_Marksmanship%fightstyle% q
+RelicSimulation.rb Hunter T21_Hunter_Marksmanship-Sentinel Hunter_Marksmanship %fightstyle% q
 RelicSimulation.rb Hunter T21_Hunter_Survival Hunter_Survival %fightstyle% q
 
 REM T20
