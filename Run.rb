5cc333627ada2478a0e6ec3b1dceaab7fdfca899
Florian Meißner <developer@mystler.eu>
2018-07-13 16:21:09 +0200

    [HAX] Ruby run script Nothing to see here move along. Proper dispatcher for running sims via ruby is still on the TODO.

---
+++ Run.rb
@@ -0,0 +1,9 @@
+#Temp HAAAX but it works
+
+# T21 Prepatch 8.0
+system "ruby Combinator.rb Rogue T21P_Rogue_Assassination Rogue_Prepatch T21 1T xxx00xx q"
+system "ruby Combinator.rb Rogue T21P_Rogue_Outlaw Rogue_Prepatch T21 1T x3x00xx q"
+system "ruby Combinator.rb Rogue T21P_Rogue_Subtlety Rogue_Prepatch T21 1T xxx00xx q"
+
+puts "All batch simulations done! Press enter!"
+gets
