d514e76e334bb94e9a3491b74c0a4d4a33e77930
Florian Meißner <developer@mystler.eu>
2018-08-03 14:28:31 +0200

    [Azerite] Initial version of the Azerite sim script

---
+++ AzeriteSimulation.rb
@@ -0,0 +1,169 @@
+require 'rubygems'
+require 'bundler/setup'
+require_relative 'lib/DataMaps'
+require_relative 'lib/Interactive'
+require_relative 'lib/JSONParser'
+require_relative 'lib/JSONResults'
+require_relative 'lib/Logging'
+require_relative 'lib/ReportWriter'
+require_relative 'lib/SimcConfig'
+require_relative 'lib/SimcHelper'
+
+Logging.Initialize("AzeriteSimulation")
+
+fightstyle = Interactive.SelectTemplate('Fightstyles/Fightstyle')
+classfolder = Interactive.SelectSubfolder('Templates')
+spec = Interactive.SelectFromArray('Specialization', ClassAndSpecIds[classfolder][:specs].keys)
+template = Interactive.SelectTemplate("Templates/#{classfolder}/")
+
+powerList = JSONParser.ReadFile("#{SimcConfig['ProfilesFolder']}/Azerite/AzeritePower.json")
+powerSettings = JSONParser.ReadFile("#{SimcConfig['ProfilesFolder']}/Azerite/AzeriteOptions.json")
+
+classId = ClassAndSpecIds[classfolder][:class_id]
+specId = ClassAndSpecIds[classfolder][:specs][spec]
+
+# Log all interactively set settings
+puts
+Logging.LogScriptInfo "Summarizing input:"
+Logging.LogScriptInfo "-- Class: #{classfolder}"
+Logging.LogScriptInfo "-- Profile: #{template}"
+Logging.LogScriptInfo "-- Fightstyle: #{fightstyle}"
+puts
+
+simcInput = []
+Logging.LogScriptInfo "Generating profilesets..."
+simcInput.push 'name="Template"'
+simcInput.push 'disable_azerite=items'
+simcInput.push ''
+
+simcInputLevels = []
+simcInputStacks = []
+powerList.each do |power|
+  next if !power['classesId'].include?(classId)
+  next if power['specId'] && !power['specId'].include?(specId)
+  next if powerSettings.include?(power['powerId'])
+
+  # Item Level Simulations
+  powerSettings['itemLevels'].each do |ilvl|
+    simcInputLevels.push "profileset.\"#{power['spellName']}_#{ilvl}\"+=azerite_override=#{power['powerId']}:#{ilvl}"
+  end
+
+  # Stack Simulations
+  (1..3).each do |stacks|
+    powerstring = (["#{power['powerId']}:#{powerSettings['baseItemLevel']}"] * stacks).join('/')
+    simcInputStacks.push "profileset.\"#{power['spellName']}_#{stacks}\"+=azerite_override=#{powerstring}"
+  end
+end
+
+##################################
+# Process Item Level simulations #
+##################################
+
+simulationFilename = "AzeriteLevels_#{fightstyle}_#{template}"
+params = [
+  "#{SimcConfig['ConfigFolder']}/SimcAzeriteConfig.simc",
+  "#{SimcConfig['ProfilesFolder']}/Fightstyles/Fightstyle_#{fightstyle}.simc",
+  "#{SimcConfig['ProfilesFolder']}/Templates/#{classfolder}/#{template}.simc",
+  simcInput + simcInputLevels,
+]
+SimcHelper.RunSimulation(params, simulationFilename)
+
+# Read JSON Output
+results = JSONResults.new(simulationFilename)
+
+# Process results
+Logging.LogScriptInfo "Processing results..."
+templateDPS = 0
+iLevelList = []
+sims = {}
+results.getAllDPSResults().each do |name, dps|
+  if data = /\A(.+)_(\p{Digit}+)\Z/.match(name)
+    sims[data[1]] = {} unless sims[data[1]]
+    sims[data[1]][data[2].to_i] = dps
+    iLevelList.push(data[2].to_i)
+  elsif name == 'Template'
+    templateDPS = dps
+  end
+end
+iLevelList.uniq!
+iLevelList.sort!
+
+# Construct the report
+Logging.LogScriptInfo "Construct the report..."
+report = []
+# Header
+header = ["Trait"]
+iLevelList.each do |ilvl|
+  header.push(ilvl.to_s)
+end
+report.push(header)
+# Body
+sims.each do |name, values|
+  actor = []
+  actor.push(name)
+  iLevelList.each do |ilvl|
+    if values[ilvl]
+      actor.push(values[ilvl] - templateDPS)
+    else
+      actor.push(0)
+    end
+  end
+  report.push(actor)
+end
+
+# Write the report(s)
+ReportWriter.WriteArrayReport(results, report)
+
+#############################
+# Process Stack Simulations # (yes, welcome to copy pasta land, deal with it)
+#############################
+
+simulationFilename = "AzeriteStacks_#{fightstyle}_#{template}"
+params = [
+  "#{SimcConfig['ConfigFolder']}/SimcAzeriteConfig.simc",
+  "#{SimcConfig['ProfilesFolder']}/Fightstyles/Fightstyle_#{fightstyle}.simc",
+  "#{SimcConfig['ProfilesFolder']}/Templates/#{classfolder}/#{template}.simc",
+  simcInput + simcInputStacks,
+]
+SimcHelper.RunSimulation(params, simulationFilename)
+
+# Read JSON Output
+results = JSONResults.new(simulationFilename)
+
+# Process results
+Logging.LogScriptInfo "Processing results..."
+templateDPS = 0
+sims = {}
+results.getAllDPSResults().each do |name, dps|
+  if data = /\A(.+)_(\p{Digit}+)\Z/.match(name)
+    sims[data[1]] = {} unless sims[data[1]]
+    sims[data[1]][data[2].to_i] = dps
+  elsif name == 'Template'
+    templateDPS = dps
+  end
+end
+
+# Construct the report
+Logging.LogScriptInfo "Construct the report..."
+report = []
+# Header
+report.push ["Trait", "1", "2", "3"]
+# Body
+sims.each do |name, values|
+  actor = []
+  actor.push(name)
+  (1..3).each do |stacks|
+    if values[stacks]
+      actor.push(values[stacks] - templateDPS)
+    else
+      actor.push(0)
+    end
+  end
+  report.push(actor)
+end
+
+# Write the report(s)
+ReportWriter.WriteArrayReport(results, report)
+
+Logging.LogScriptInfo 'Done! Press enter to quit...'
+Interactive.GetInputOrArg()
---
+++ lib/DataMaps.rb
@@ -0,0 +1,100 @@
+# This file contains lookup tables for data we use when parsing things.
+
+ClassAndSpecIds = {
+  'Warrior' => {
+    class_id: 1,
+    specs: {
+      'Arms' => 71,
+      'Fury' => 72,
+      'Protection' => 73,
+    },
+  },
+  'Paladin' => {
+    class_id: 2,
+    specs: {
+      'Holy' => 65,
+      'Protection' => 66,
+      'Retribution' => 70,
+    },
+  },
+  'Hunter' => {
+    class_id: 3,
+    specs: {
+      'BeastMastery' => 253,
+      'Marksmanship' => 254,
+      'Survival' => 255,
+    },
+  },
+  'Rogue' => {
+    class_id: 4,
+    specs: {
+      'Assassination' => 259,
+      'Outlaw' => 260,
+      'Subtlety' => 261,
+    },
+  },
+  'Priest' => {
+    class_id: 5,
+    specs: {
+      'Discipline' => 256,
+      'Holy' => 257,
+      'Shadow' => 258,
+    },
+  },
+  'DeathKnight' => {
+    class_id: 6,
+    specs: {
+      'Blood' => 250,
+      'Frost' => 251,
+      'Unholy' => 252,
+    },
+  },
+  'Shaman' => {
+    class_id: 7,
+    specs: {
+      'Elemental' => 262,
+      'Enhancement' => 263,
+      'Restoration' => 264,
+    },
+  },
+  'Mage' => {
+    class_id: 8,
+    specs: {
+      'Arcane' => 62,
+      'Fire' => 63,
+      'Frost' => 64,
+    },
+  },
+  'Warlock' => {
+    class_id: 9,
+    specs: {
+      'Affliction' => 265,
+      'Demonology' => 266,
+      'Destruction' => 267,
+    },
+  },
+  'Monk' => {
+    class_id: 10,
+    specs: {
+      'Brewmaster' => 268,
+      'Windwalker' => 269,
+      'Mistweaver' => 270,
+    },
+  },
+  'Druid' => {
+    class_id: 11,
+    specs: {
+      'Balance' => 102,
+      'Feral' => 103,
+      'Guardian' => 104,
+    },
+    'Restoration' => 105,
+  },
+  'DemonHunter' => {
+    class_id: 12,
+    specs: {
+      'Havoc' => 577,
+      'Vengeance' => 581,
+    },
+  },
+}
