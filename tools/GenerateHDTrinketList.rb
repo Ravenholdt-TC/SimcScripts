1e26e4d63d3a84c2b23baaee2d93e914d570c71e
Quentin Giraud <dev@aethys.io>
2018-08-19 01:29:45 +0200

    fix: GenerateHDTrinketList.rb json paths

--- tools/GenerateHDTrinketList.rb
+++ tools/GenerateHDTrinketList.rb
@@ -1,9 +1,6 @@
 require_relative '../lib/SimcConfig'
 require_relative '../lib/JSONParser'
 
-powerList = JSONParser.ReadFile("#{SimcConfig['ProfilesFolder']}/Azerite/AzeritePower.json")
-powerSettings = JSONParser.ReadFile("#{SimcConfig['ProfilesFolder']}/Azerite/AzeriteOptions.json")
-
 allTrinkets = []
 files = Dir.glob("#{SimcConfig['ProfilesFolder']}/TrinketLists/[_\-+a-zA-Z0-9]*\.json")
 files.sort!
@@ -12,7 +9,7 @@ files.each do |file|
   allTrinkets += data['trinkets']
 end
 
-JSONParser.WritePrettyFile("#{SimcConfig['HeroDamagePath']}/src/assets/wow-data/TrinketRaw.json", allTrinkets)
+JSONParser.WritePrettyFile("#{SimcConfig['HeroDamagePath']}/src/assets/wow-data/raw/Trinket.json", allTrinkets)
 
 puts
 puts "Done!"
