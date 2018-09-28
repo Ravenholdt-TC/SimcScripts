c1e37d46f89689cd15a087bb069d616d24581da5
Florian Meißner <developer@mystler.eu>
2018-09-28 16:25:35 +0200

    [Race] Implement Combination based sims + Reformat

--- RaceSimulation.rb
+++ RaceSimulation.rb
@@ -1,18 +1,26 @@
-require 'rubygems'
-require 'bundler/setup'
-require_relative 'lib/DataMaps'
-require_relative 'lib/Interactive'
-require_relative 'lib/JSONResults'
-require_relative 'lib/Logging'
-require_relative 'lib/ReportWriter'
-require_relative 'lib/SimcConfig'
-require_relative 'lib/SimcHelper'
+require "rubygems"
+require "bundler/setup"
+require_relative "lib/DataMaps"
+require_relative "lib/HeroInterface"
+require_relative "lib/Interactive"
+require_relative "lib/JSONResults"
+require_relative "lib/Logging"
+require_relative "lib/ReportWriter"
+require_relative "lib/SimcConfig"
+require_relative "lib/SimcHelper"
 
 Logging.Initialize("RaceSimulation")
 
-fightstyle, fightstyleFile = Interactive.SelectTemplate('Fightstyles/Fightstyle_')
-classfolder = Interactive.SelectSubfolder('Templates')
-template, templateFile = Interactive.SelectTemplate(["Templates/#{classfolder}/", ''], classfolder)
+fightstyle, fightstyleFile = Interactive.SelectTemplate("Fightstyles/Fightstyle_")
+classfolder = Interactive.SelectSubfolder("Templates")
+template, templateFile = Interactive.SelectTemplate(["Templates/#{classfolder}/", ""], classfolder)
+
+# Read talents from template profile
+talents = ProfileHelper.GetValueFromTemplate("talents", templateFile)
+unless talents
+  Logging.LogScriptError "No talents= string found in profile!"
+  exit
+end
 
 # Log all interactively set settings
 puts
@@ -26,18 +34,30 @@ simcInput = []
 Logging.LogScriptInfo "Generating profilesets..."
 simcInput.push 'name="Template"'
 simcInput.push 'race=""'
-simcInput.push ''
-RaceInputMap.each do |name, raceString|
-  if ClassRaceMap[classfolder].include?(name)
-    prefix = "profileset.\"#{name}\"+="
-    simcInput.push(prefix + "name=\"#{name}\"")
-    simcInput.push(prefix + "race=#{raceString}")
+simcInput.push ""
+
+# Get better combination overrides if CombinationBasedCharts is enabled. These will be run in addition to defaults.
+combinationOverrides = HeroInterface.GetBestCombinationOverrides(fightstyle, template, talents)
+# Add empty override set for the default loop
+combinationOverrides[nil] = []
+
+combinationOverrides.each do |optionsString, overrides|
+  RaceInputMap.each do |name, raceString|
+    if ClassRaceMap[classfolder].include?(name)
+      psetName = "#{name}#{"--" if optionsString}#{optionsString}"
+      prefix = "profileset.\"#{psetName}\"+="
+      simcInput.push(prefix + "name=\"#{psetName}\"")
+      simcInput.push(prefix + "race=#{raceString}")
+      overrides.each do |override|
+        simcInput.push(prefix + "#{override}")
+      end
+    end
   end
 end
 
 simulationFilename = "RaceSimulation_#{fightstyle}_#{template}"
 params = [
-  "#{SimcConfig['ConfigFolder']}/SimcRaceConfig.simc",
+  "#{SimcConfig["ConfigFolder"]}/SimcRaceConfig.simc",
   fightstyleFile,
   templateFile,
   simcInput,
@@ -50,8 +70,8 @@ results = JSONResults.new(simulationFilename)
 # Process results
 Logging.LogScriptInfo "Processing results..."
 sims = results.getAllDPSResults()
-templateDPS = sims['Template'] or 0
-sims.delete('Template')
+templateDPS = sims["Template"] or 0
+sims.delete("Template")
 
 # Construct the report
 Logging.LogScriptInfo "Construct the report..."
@@ -72,5 +92,5 @@ end
 # Write the report(s)
 ReportWriter.WriteArrayReport(results, report)
 
-Logging.LogScriptInfo 'Done! Press enter to quit...'
+Logging.LogScriptInfo "Done! Press enter to quit..."
 Interactive.GetInputOrArg()
