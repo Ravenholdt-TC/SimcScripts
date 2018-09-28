e7a860feda94518ee89ac95e81b963fd3a7785a7
Florian Meißner <developer@mystler.eu>
2018-09-28 16:20:33 +0200

    [Lib] Refactor some HeroInterface code and add GetBestCombinationOverrides In this and the following commits my autoformatter surprised be with changes i did not notice at first and was to lazy to roll back manually. So I am going to update format going forward.

--- lib/HeroInterface.rb
+++ lib/HeroInterface.rb
@@ -1,82 +1,78 @@
-require_relative 'SimcConfig'
-require_relative 'JSONParser'
-require_relative 'Logging'
-require_relative 'ProfileHelper'
-require 'git'
+require_relative "SimcConfig"
+require_relative "JSONParser"
+require_relative "Logging"
+require_relative "ProfileHelper"
+require "git"
 
 module HeroInterface
-  @@updatedHD = false
+  @updatedHD = false
 
-  # Create an array of azeritePowerName from genericCombinatorPowers to exclude them from the results
-  @@genericPowers = []
-  @@powerList = JSONParser.ReadFile("#{SimcConfig['ProfilesFolder']}/Azerite/AzeritePower.json")
-  @@powerSettings = JSONParser.ReadFile("#{SimcConfig['ProfilesFolder']}/Azerite/AzeriteOptions.json")
-  @@powerList.each do |power|
-    @@genericPowers.push(power['spellName']) if @@powerSettings['genericCombinatorPowers'].include?(power['powerId'])
-  end
+  TargetErrorDiffFactor = 2
 
   # Check and update HeroDamage repo. Returns false if no repo found or exceptions occur.
   def self.PullLatest
-    return true if @@updatedHD
+    return true if @updatedHD
     begin
-      Logging.LogScriptInfo "Updating HD repo at #{SimcConfig['HeroDamagePath']}..."
-      g = Git.open(SimcConfig['HeroDamagePath'])
+      Logging.LogScriptInfo "Updating HD repo at #{SimcConfig["HeroDamagePath"]}..."
+      g = Git.open(SimcConfig["HeroDamagePath"])
       g.pull
-      @@updatedHD = true
+      @updatedHD = true
     rescue => err
-      Logging.LogScriptError 'Error updating HeroDamage repository. Make sure the path is set and it is a git repository.'
+      Logging.LogScriptError "Error updating HeroDamage repository. Make sure the path is set and it is a git repository."
       Logging.LogScriptError err
       return false
     end
     return true
   end
 
-  # Get best talent builds for the Azerite sim at given stacks per power using Combinations results.
-  def self.GetAzeriteCombinations(stacks, fightstyle, defaultTalents, profile)
-    return nil unless SimcConfig['CombinationBasedAzeriteCharts']
-    unless SimcConfig['HeroOutput']
-      Logging.LogScriptError 'HeroOutput option off with CombinationBasedAzeriteCharts on! This may cause problems!'
-    end
-    unless stacks > 0
-      Logging.LogScriptError '0 stack AzeriteCombinations called! This may cause problems since they are always used!'
-    end
-
-    simulationFilename = ProfileHelper.NormalizeProfileName(profile)
-
-    # First look if there is a local report then checks HeroDamage repository.
-    combinationsFile = "Combinator-#{stacks}A_#{fightstyle}_#{profile}.json"
-    combinationsGenericFile = "Combinator-0A_#{fightstyle}_#{profile}.json"
-    localPath = "#{SimcConfig['ReportsFolder']}/#{combinationsFile}"
-    localGenericPath = "#{SimcConfig['ReportsFolder']}/#{combinationsGenericFile}"
-    hdPath = "#{SimcConfig['HeroDamagePath']}/#{SimcConfig['HeroDamageReportsFolder']}/#{combinationsFile}"
-    hdGenericPath = "#{SimcConfig['HeroDamagePath']}/#{SimcConfig['HeroDamageReportsFolder']}/#{combinationsGenericFile}"
-    if File.exist?(localPath) && File.exist?(localGenericPath)
+  def self.GetRawCombinationData(azeriteStacks, fightstyle, profile)
+    # First look if there is a local report, then check HeroDamage repository.
+    combinationsFile = "Combinator-#{azeriteStacks}A_#{fightstyle}_#{profile}.json"
+    localPath = "#{SimcConfig["ReportsFolder"]}/#{combinationsFile}"
+    hdPath = "#{SimcConfig["HeroDamagePath"]}/#{SimcConfig["HeroDamageReportsFolder"]}/#{combinationsFile}"
+    if File.exist?(localPath)
       Logging.LogScriptInfo "Reading combinations file #{localPath}..."
       data = JSONParser.ReadFile(localPath)
-      Logging.LogScriptInfo "Reading combinations file #{localGenericPath}..."
-      genericData = JSONParser.ReadFile(localGenericPath)
-    elsif PullLatest() && File.exist?(hdPath) && File.exist?(hdGenericPath)
+    elsif PullLatest() && File.exist?(hdPath)
       Logging.LogScriptInfo "Reading combinations file #{hdPath}..."
       data = JSONParser.ReadFile(hdPath)
-      Logging.LogScriptInfo "Reading combinations file #{hdGenericPath}..."
-      genericData = JSONParser.ReadFile(hdGenericPath)
     else
-      Logging.LogScriptFatal "File #{combinationsFile} or #{combinationsGenericFile} not found for combinations."
+      Logging.LogScriptFatal "File #{combinationsFile} not found for combinations."
       exit
+    end
+    return data
+  end
+
+  # Get best talent builds for the Azerite sim at given stacks per power using Combinations results.
+  def self.GetAzeriteCombinations(fightstyle, profile, defaultTalents)
+    return {} unless SimcConfig["CombinationBasedCharts"]
+    unless SimcConfig["HeroOutput"]
+      Logging.LogScriptError "HeroOutput option off with CombinationBasedCharts on! This may cause problems!"
+    end
+
+    profile = ProfileHelper.NormalizeProfileName(profile)
+    data = GetRawCombinationData(3, fightstyle, profile)
+    genericData = GetRawCombinationData(0, fightstyle, profile)
+
+    # Create an array of azeritePowerName from genericCombinatorPowers to exclude them from the results
+    genericPowers = []
+    powerList = JSONParser.ReadFile("#{SimcConfig["ProfilesFolder"]}/Azerite/AzeritePower.json")
+    powerSettings = JSONParser.ReadFile("#{SimcConfig["ProfilesFolder"]}/Azerite/AzeriteOptions.json")
+    powerList.each do |power|
+      genericPowers.push(power["spellName"]) if powerSettings["genericCombinatorPowers"].include?(power["powerId"])
     end
 
     # Combinations results array mapping:
     # result: 0-rank, 1-talents, 2-set, 3-powerName, 4-dps
 
     # Filter the combinations results to only keep non-generic powers
-    filteredResults = data['results'].select { |result| !@@genericPowers.include?(result[3]) }
+    filteredResults = data["results"].select { |result| !genericPowers.include?(result[3]) }
 
     # Create a map of azeritePowerName => DPS using default talent build
-
     defaultResults = filteredResults.select { |result| result[1] == defaultTalents }
     defaultTalentsFound = false
     if defaultResults.empty?
-      Logging.LogScriptWarning "defaultTalents were not found in #{combinationsFile}, make sure the default build is included to make best usage of this feature."
+      Logging.LogScriptWarning "Default talents #{defaultTalents} were not found, make sure the default build is included to make best usage of this feature."
     else
       defaultTalentsFound = true
       defaultMatchedData = {}
@@ -88,8 +84,8 @@ module HeroInterface
     end
 
     # Set up threshold for comparison based on target error
-    targetError = [data['metas']['targetError'], genericData['metas']['targetError']].max
-    minimalDifferenceFromDefaults = targetError * 2
+    targetError = [data["metas"]["targetError"], genericData["metas"]["targetError"]].max
+    minimalDifferenceFromDefaults = targetError * TargetErrorDiffFactor
 
     # Create a map of azeritePowerName => talent build using best DPS
     matchedData = {}
@@ -118,19 +114,55 @@ module HeroInterface
 
     # Find the best performing talents build for generics (i.e. no specific power)
     if defaultTalentsFound
-      defaultGenericResult = genericData['results'].select { |result| result[1] == defaultTalents }.first
-      topGenericResult = genericData['results'].first
+      defaultGenericResult = genericData["results"].select { |result| result[1] == defaultTalents }.first
+      topGenericResult = genericData["results"].first
       defaultGenericDPS = defaultGenericResult[4]
       topGenericDPS = topGenericResult[4]
       genericDifference = 100 * topGenericDPS.to_f / defaultGenericDPS.to_f - 100
       if genericDifference > minimalDifferenceFromDefaults
-        matchedData['Generic'] = topGenericResult[1]
+        matchedData["Generic"] = topGenericResult[1]
         Logging.LogScriptInfo "Generic: #{topGenericResult[1]} is better than #{defaultTalents} by #{genericDifference.round(2)}% (#{topGenericDPS} vs #{defaultGenericDPS})."
       else
-        matchedData['Generic'] = defaultTalents
+        matchedData["Generic"] = defaultTalents
       end
     end
 
     return matchedData
+  end
+
+  # Get best talent + azerite build overrides (if better) based on Combinations results.
+  def self.GetBestCombinationOverrides(fightstyle, profile, defaultTalents)
+    return {} unless SimcConfig["CombinationBasedCharts"]
+    unless SimcConfig["HeroOutput"]
+      Logging.LogScriptError "HeroOutput option off with CombinationBasedCharts on! This may cause problems!"
+    end
+
+    profile = ProfileHelper.NormalizeProfileName(profile)
+    genericData = GetRawCombinationData(0, fightstyle, profile)
+
+    # Map of override sets by options name (part including and after --)
+    # This can easily extended if we want to support multiple sets.
+    overrideSets = {}
+
+    # Set up threshold for comparison based on target error
+    targetError = genericData["metas"]["targetError"]
+    minimalDifferenceFromDefaults = targetError * TargetErrorDiffFactor
+
+    # Combinations results array mapping:
+    # result: 0-rank, 1-talents, 2-set, 3-powerName, 4-dps
+
+    # Get the best combination without azerite (if better) and create an override set for it
+    best = genericData["results"].first
+    default = genericData["results"].find { |x| x[1] == defaultTalents }
+    if best && default && best[1] != default[1]
+      difference = 100 * best[4].to_f / default[4].to_f - 100
+      if difference > minimalDifferenceFromDefaults
+        Logging.LogScriptInfo "0A: Talent build #{best[1]} beats #{default[1]} by #{difference.round(2)}% (#{best[4]} vs #{default[4]})."
+        overrideSets["talents:#{best[1]}"] = ["talents=#{best[1]}"]
+      end
+    elsif !default
+      Logging.LogScriptWarning "Default talents #{defaultTalents} were not found, make sure the default build is included to make best usage of this feature."
+    end
+    return overrideSets
   end
 end
