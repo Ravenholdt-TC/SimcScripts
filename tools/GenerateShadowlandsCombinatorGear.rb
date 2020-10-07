f54d6721de1e15c91dca7caea2116e094bd1c9e1
Florian Meißner <developer@mystler.eu>
2020-10-07 17:05:48 +0200

    [Tools] Use one generator for legendaries and conduits

---
+++ tools/GenerateShadowlandsCombinatorGear.rb
@@ -0,0 +1,39 @@
+require_relative "../lib/DataMaps"
+require_relative "../lib/JSONParser"
+
+legoList = JSONParser.ReadFile("#{SimcConfig["ProfilesFolder"]}/Legendaries.json")
+conduitList = JSONParser.ReadFile("#{SimcConfig["ProfilesFolder"]}/Conduits.json")
+
+ClassAndSpecIds.keys.each do |classStr|
+  legoGear = {"specials" => {}, "sets" => {"None" => {}}}
+  conduitGear = {"specials" => {}, "sets" => {"None" => {}}}
+  fullGear = {"specials" => {}, "sets" => {"None" => {}}}
+
+  ClassAndSpecIds[classStr][:specs].each do |specName, specId|
+    legoGear["specials"][specName] = {"shirt" => {}}
+    legoList.each do |lego|
+      next if !lego["specs"].include?(specId)
+      legoName = lego["legendaryName"]
+      legoGear["specials"][specName]["shirt"][legoName] = "sl_legendary,bonus_id=#{lego["legendaryBonusID"]}"
+    end
+    fullGear["specials"][specName] = legoGear["specials"][specName].clone
+
+    conduitGear["specials"][specName] = {"conduit" => {}}
+    conduitList.each do |conduit|
+      next if !conduit["specs"].include?(specId)
+
+      itemName = conduit["conduitName"]
+      conduitGear["specials"][specName]["conduit"][itemName] = "#{conduit["conduitId"]}:!!RANK!!"
+    end
+    fullGear["specials"][specName].merge!(conduitGear["specials"][specName])
+  end
+  puts "Writing Legendaries for #{classStr}..."
+  JSONParser.WritePrettyFile("#{SimcConfig["ProfilesFolder"]}/Combinator/#{classStr}/CombinatorGear_#{classStr}_Legendaries.json", legoGear)
+  puts "Writing Conduits for #{classStr}..."
+  JSONParser.WritePrettyFile("#{SimcConfig["ProfilesFolder"]}/Combinator/#{classStr}/CombinatorGear_#{classStr}_Conduits.json", conduitGear)
+  puts "Writing Full gear for #{classStr}..."
+  JSONParser.WritePrettyFile("#{SimcConfig["ProfilesFolder"]}/Combinator/#{classStr}/CombinatorGear_#{classStr}_ShadowlandsFull.json", fullGear)
+end
+
+puts
+puts "Done!"
