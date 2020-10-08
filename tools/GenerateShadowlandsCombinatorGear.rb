dc9b19483a68c840f9c6dd10e355a389a111a409
Florian Meißner <developer@mystler.eu>
2020-10-08 16:07:32 +0200

    Merge covenants into SL generator and patch Legendaries

--- tools/GenerateShadowlandsCombinatorGear.rb
+++ tools/GenerateShadowlandsCombinatorGear.rb
@@ -9,6 +9,8 @@ ClassAndSpecIds.keys.each do |classStr|
   conduitGear = {"specials" => {}, "sets" => {"None" => {}}}
   fullGear = {"specials" => {}, "sets" => {"None" => {}}}
 
+  covenants = {"specials" => {}, "sets" => {"None" => {}}}
+
   ClassAndSpecIds[classStr][:specs].each do |specName, specId|
     legoGear["specials"][specName] = {"shirt" => {}}
     legoList.each do |lego|
@@ -27,6 +29,13 @@ ClassAndSpecIds.keys.each do |classStr|
       conduitGear["specials"][specName]["conduit"][itemName] = "#{conduit["conduitId"]}:!!RANK!!"
     end
     fullGear["specials"][specName].merge!(conduitGear["specials"][specName])
+
+    covenants["specials"][specName] = {"covenant" => {
+      "Kyrian" => "kyrian",
+      "Necrolord" => "necrolord",
+      "Night Fae" => "night_fae",
+      "Venthyr" => "venthyr",
+    }}
   end
   puts "Writing Legendaries for #{classStr}..."
   JSONParser.WritePrettyFile("#{SimcConfig["ProfilesFolder"]}/Combinator/#{classStr}/CombinatorGear_#{classStr}_Legendaries.json", legoGear)
@@ -34,6 +43,8 @@ ClassAndSpecIds.keys.each do |classStr|
   JSONParser.WritePrettyFile("#{SimcConfig["ProfilesFolder"]}/Combinator/#{classStr}/CombinatorGear_#{classStr}_Conduits.json", conduitGear)
   puts "Writing Full gear for #{classStr}..."
   JSONParser.WritePrettyFile("#{SimcConfig["ProfilesFolder"]}/Combinator/#{classStr}/CombinatorGear_#{classStr}_ShadowlandsFull.json", fullGear)
+  puts "Writing Covenants for #{classStr}..."
+  JSONParser.WritePrettyFile("#{SimcConfig["ProfilesFolder"]}/Combinator/#{classStr}/CombinatorGear_#{classStr}_Covenants.json", covenants)
 end
 
 puts
